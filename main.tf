460ba4abc712a2d5bb823b258c4fb682ca5e345d
dgouvea <david@pairingwith.me>
2024-08-21 22:45:50 -0700

    fix issue

--- main.tf
+++ main.tf
@@ -31,18 +31,11 @@ module "lambda_authorizer" {
 module "lambda_path" {
   source = "./modules/lambda"
 
-  for_each = toset(flatten([for path, methods in var.api_paths : [for method, config in methods : {
-    path                           = path
-    method                         = method
-    function_file                  = config.lambda.file
-    function_zip                   = config.lambda.zip
-    function_runtime               = config.lambda.runtime
-    function_memory_size           = config.lambda.memory_size
-    function_timeout               = config.lambda.timeout
-    function_environment_variables = config.lambda.environment_variables
-    function_policies              = config.lambda.policies
-    function_log_retention         = config.lambda.log_retention
-  }]]))
+  for_each = flatten([for path, methods in var.api_paths : [for method, config in methods : {
+    path   = path
+    method = method
+    config = config
+  }]])
 
   env                            = var.env
   function_file                  = each.value.function_file
