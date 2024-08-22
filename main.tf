4349eca7af9f0f370d294da5c651157377bd30b6
dgouvea <david@pairingwith.me>
2024-08-21 21:58:56 -0700

    fix issue

--- main.tf
+++ main.tf
@@ -266,7 +266,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
 }
 
 resource "aws_api_gateway_stage" "api_stage" {
-  depends_on = [aws_api_gateway_deployment.api]
+  depends_on = [aws_api_gateway_deployment.api_deployment]
 
   rest_api_id   = aws_api_gateway_rest_api.api.id
   deployment_id = aws_api_gateway_deployment.api_deployment.id
