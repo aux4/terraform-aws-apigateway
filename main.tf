e777f76905d2dae4f1cd388a81a17cb1d1654348
dgouvea <david@pairingwith.me>
2025-06-10 23:50:24 -0700

    include cookie in the headers

--- main.tf
+++ main.tf
@@ -233,7 +233,7 @@ resource "aws_api_gateway_rest_api" "api" {
                   responseParameters = {
                     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
                     "method.response.header.Access-Control-Allow-Methods" = "'${join(",", concat(["OPTIONS"], distinct(flatten([for method, config in methods : upper(method)]))))}'"
-                    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+                    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Access-Control-Allow-Credentials,Cookie'"
                   }
                 }
               }
